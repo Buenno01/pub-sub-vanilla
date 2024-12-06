c472b05d77f844df500f012470b5c7b7efb161a5
Vinícius Bueno Costa <buenov535@gmail.com>
2024-12-05 21:45:05 -0300

    feat: added update UI function

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -24,5 +24,24 @@ if (!customElements.get('cart-drawer')) {
     openCart() {
       this.classList.add('active');
     }
+
+    async updateUI() {
+      try {
+        const response = await fetch (location.href);
+
+        if (!response.ok) {
+          throw new Error('Erro ao atualizar o carrinho');
+        }
+
+        const body = await response.text();
+        const parser = new DOMParser();
+        const newDocument = parser.parseFromString(body, 'text/html');
+        const cartDrawer = newDocument.querySelector('cart-drawer');
+
+        this.innerHTML = cartDrawer.innerHTML;
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
   });
 }
